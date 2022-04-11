05f86e9fc98ae980f5cf61926507e1570d92f3e6
Alex <ateal3235@gmail.com>
2022-04-11 16:04:58 -0400

    fixed. Possible mem leaks and bugs to squash

--- veronica/network_watcher.c
+++ veronica/network_watcher.c
@@ -4,6 +4,87 @@
 #include <unistd.h>
 #include <signal.h>
 #include <errno.h>
+
+
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h> /* for strncpy */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+#include <netdb.h>
+#include <ifaddrs.h>
+
+
+
+
+//stolen code
+char* get_ip(char* host){
+    /* int fd;
+       struct ifreq ifr;
+
+       fd = socket(AF_INET, SOCK_DGRAM, 0);
+       */
+    /* I want to get an IPv4 IP address */
+    //ifr.ifr_addr.sa_family = AF_INET;
+
+    /* I want IP address attached to "eth0" */
+    //strncpy(ifr.ifr_name, "inet", IFNAMSIZ-1);
+
+    //ioctl(fd, SIOCGIFBRDADDR, &ifr);
+
+    //close(fd);
+
+    /* display result */
+    //printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+
+    //return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+    //
+
+
+    struct ifaddrs *ifaddr, *ifa;
+    int s;
+
+    if (getifaddrs(&ifaddr) == -1)
+    {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        s=getnameinfo(ifa->ifa_addr,sizeof(struct sockaddr_in),host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+
+        if( (strcmp(ifa->ifa_name,"en0")==0)&&(  ifa->ifa_addr->sa_family==AF_INET)  )
+        {
+            if (s != 0)
+            {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+            printf("\tInterface : <%s>\n",ifa->ifa_name );
+            printf("\t  Address : <%s>\n", host);
+            break;
+        }
+    }
+    freeifaddrs(ifaddr);
+    
+    return host;
+}
+
+
+
 //initiate scutil child process to watch for ipv4 changes
 //for now, scutil should be killed by init
 //Lets just assume on parent's exit, this child is killed
@@ -96,12 +177,13 @@ int startup(){
     pipe[0] = scutil_listener(pipe,&cpid); //handle pipe? fd[0] is read and open
 
 
-        fflush(stdout);
+    fflush(stdout);
     //stolen code
     sleep(2); // wait for scutil to launch
+    int nread;
 
     while(1){
-        int nread = read(pipe[0], output, 1024);
+        nread = read(pipe[0], output, 1024);
         output[1024] = '\0';
         switch (nread) {
             case -1:
@@ -128,8 +210,14 @@ int startup(){
                 // by default return no. of bytes
                 // which read call read at that time
                 printf("scutil says \"%s \"\n", output);
-
-
+                char host[NI_MAXHOST];
+                get_ip(host);
+                printf(" ip is %s \n",host);
+                /*
+                 *
+                 *  insert ip file update here
+                 *
+                 */
 
         }
         sleep(5);
